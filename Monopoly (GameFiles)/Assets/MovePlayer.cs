4bfbfece0be6ad282d3fe5df34379acd1520cb3c
jenso2000 <45063123+jenso2000@users.noreply.github.com>
2018-12-13 11:14:13 +0100

    Speler kan bewegen en tekst
    De kubus kan nu bewegen. HIj beweegt een random aantal van 2 t/m 12. Links boven staat wat je hebt gegooi. Dit is een verandering aan de back end.
    
    Co-Authored-By: daankahmann <daankahmann@users.noreply.github.com>

---
+++ Monopoly (GameFiles)/Assets/MovePlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovePlayer : MonoBehaviour {
+	public int steps;
+	private int dice;
+	public Text diceText;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("down")) {
+			int steps = Random.Range(2,12);
+			transform.Translate (steps*2,0,0);
+			diceText.text = "Je hebt gegooid: " + steps.ToString ();
+		}
+	}
+}
