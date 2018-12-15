bce323dd50543ee93adab7faded6e9d9d8f3921d
jenso2000 <45063123+jenso2000@users.noreply.github.com>
2018-12-15 15:45:02 +0100

    Eerste probeersel positie as swap

--- Monopoly (GameFiles)/Assets/MovePlayer.cs
+++ Monopoly (GameFiles)/Assets/MovePlayer.cs
@@ -7,6 +7,7 @@ public class MovePlayer : MonoBehaviour {
 	public int steps;
 	private int dice;
 	public Text diceText;
+	private transform.position.z = GetAxis("Z");
 	// Use this for initialization
 	void Start () {
 		
@@ -14,10 +15,12 @@ public class MovePlayer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown("down")) {
+		if (Input.GetKeyDown("space") && transform.position.z > -45 ) {
 			int steps = Random.Range(2,12);
-			transform.Translate (steps*2,0,0);
+			transform.Translate (0,0,steps*-10);
 			diceText.text = "Je hebt gegooid: " + steps.ToString ();
+		} else if (Input.GetKeyDown("space") && transform.position.z < -45) {
+			transform.Translate (steps*-10,0,0);
 		}
 	}
 }
