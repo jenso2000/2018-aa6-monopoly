b4f89b201d70306e71385fd342d60cee9b4d9c9d
bram859 <45063419+bram859@users.noreply.github.com>
2018-12-18 14:18:42 +0100

    Wij hebben een variable geld gemaakt
    De beginhoeveelheid is 1500 en dit is zichtbaar op het scherm. Ook is de speler verplaatst uit de materials.
    
    Co-Authored-By: daankahmann <daankahmann@users.noreply.github.com>

---
+++ Monopoly (GameFiles)/Assets/Scripts/GeldScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GeldScript : MonoBehaviour {
+	public Text geldText;
+	public int geldAantal;
+
+	// Use this for initialization
+	void Start () {
+		geldAantal = 1500;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		geldText.text = "Geld " + geldAantal;
+	}
+}
