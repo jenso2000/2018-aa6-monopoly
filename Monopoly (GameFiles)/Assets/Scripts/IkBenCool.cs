e582d72824a06a202be29ddce384df50a1f1dc99
bram859 <45063419+bram859@users.noreply.github.com>
2018-12-19 19:15:09 +0100

    Ik heb een beetje geprutst
    Conclusie het is het niet waard om met animaties bezig te houden, veel te veel moeite

---
+++ Monopoly (GameFiles)/Assets/Scripts/IkBenCool.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IkBenCool : MonoBehaviour {
+
+    public int KillerSteps;
+    private int Killerdice;
+
+    // Use this for initialization
+    void Start () {
+        Actions actions = GetComponent<Actions>();
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input)
+
+
+
+
+        if (Input.GetKeyDown("space"))
+        {
+            print("test1");
+            int KillerSteps = Random.Range(2, 12);
+            for (int i = KillerSteps; i > 0; i--)
+            {
+                print(transform.position.z);
+                if (transform.position.z > -45 && transform.position.x == 55)
+                {
+                    transform.Translate(0, 0, -10);
+                }
+                else if (transform.position.z == -45 && transform.position.x > -45)
+                {
+                    print("test2");
+                    transform.Translate(-10, 0, 0);
+                }
+                else if (transform.position.z < 55 && transform.position.x == -45)
+                {
+                    transform.Translate(0, 0, 10);
+                }
+                else if (transform.position.z == 55 && transform.position.x < 55)
+                {
+                    transform.Translate(10, 0, 0);
+                }
+            }
+        }
+    }
+}
