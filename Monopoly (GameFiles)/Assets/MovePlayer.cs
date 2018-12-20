3d55da865fd686fcfe297a1796a805f4a4995b0c
DaanKahmann <45063291+DaanKahmann@users.noreply.github.com>
2018-12-20 09:50:40 +0100

    Revert "Merge branch 'master' of https://github.com/jenso2000/2018-aa6-monopoly"
    This reverts commit 55f54fcc9d697f1d6843c1a797a49d746e620a72, reversing
    changes made to 9113b3a4d4253bc4e7818d46d9c95e555b4b6292.

---
+++ Monopoly (GameFiles)/Assets/MovePlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovePlayer : MonoBehaviour {
+	public int steps;
+	private int dice;
+	public Text diceText;
+	// Use this for initialization
+	void Start () {
+		 Vector3 position = new Vector3(transform.position.x,transform.position.y, transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("space") && transform.position.z > -45 ) {
+			int steps = Random.Range(2,12);
+			transform.Translate (0,0,steps*-10);
+			diceText.text = "Je hebt gegooid: " + steps.ToString ();
+		} else if (Input.GetKeyDown("space") && transform.position.z < -45) {
+			transform.Translate (steps*-10,0,0);
+		}
+	}
+}
