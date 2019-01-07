11ba5599e442a33c3429be060a3159e3465e4d49
DaanKahmann <45063291+DaanKahmann@users.noreply.github.com>
2019-01-07 11:19:25 +0100

    button die iets doet bij klikken

---
+++ Monopoly (GameFiles)/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour {
+	public Text buttonText;
+	public Button myButton;
+
+	// Use this for initialization
+	void Start () {
+		myButton.onClick.AddListener(TaskOnClick);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void TaskOnClick () {
+			if (transform.position.z == 45 && transform.position.x == 55) {
+			buttonText.text = "geklikt 2.0";
+		}
+		buttonText.text = "geklikt";
+	}
+}
